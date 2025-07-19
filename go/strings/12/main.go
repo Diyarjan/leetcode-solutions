5777192bd88a8997a24f32da9f1eb026ccc2f2c5
Diyarjan <diyarjan.friend.dm@gmail.com>
2025-07-19 16:43:54 +0500

    only syntax

--- go/strings/12/main.go
+++ go/strings/12/main.go
@@ -1,28 +1,31 @@
 // 9. Palindrome Number
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func isPalindrome(x int) bool {
-	// s := strconv.Itoa(x)
-	// for i := 0; i <= int(len(s)/2); i++ {
-	// 	if s[i] != s[len(s)-1-i] {
-	// 		return false
-	// 	}
-	// }
-	// return true
-
-	n := x
-	m := 0
-	for n > 0 {
-		m = m*10 + (n % 10)
-		n = int(n / 10)
+	s := strconv.Itoa(x)
+	for i := 0; i <= int(len(s)/2); i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
 	}
-	return m == x && x >= 0
+	return true
+
+	// n := x
+	// m := 0
+	// for n > 0 {
+	// 	m = m*10 + (n % 10)
+	// 	n = int(n / 10)
+	// }
+	// return m == x && x >= 0
 }
 
 func main() {
 
-	fmt.Println(isPalindrome(1131))
+	fmt.Println(isPalindrome(11511))
 
 }
